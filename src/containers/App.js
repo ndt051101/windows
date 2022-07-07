ec832697a5e9ba11ac5f7f85602fe588fb71efc0
Duy Tiến <duytienabc123@gmail.com>
2022-07-07 14:47:41 +0700

    Change name folder

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,8 +5,8 @@ import { ConnectedRouter as Router } from "connected-react-router";
 import { history } from "../redux";
 import { ToastContainer } from "react-toastify";
 import {
-  userIsAuthenticated,
-  userIsNotAuthenticated,
+    userIsAuthenticated,
+    userIsNotAuthenticated,
 } from "../hoc/authentication";
 import { path } from "../utils";
 import Home from "../routes/Home";
@@ -21,78 +21,101 @@ import DetailProject from "./Project/DetailProject";
 import GetAllProduct from "./AllData/Product/GetAllProduct";
 import GetAllProject from "./AllData/Project/GetAllProject";
 import GetAllPosts from "./AllData/Posts/GetAllPosts";
-import VerifyEmail from "./Patient/VerifyEmail";
+import VerifyEmail from "./Customer/VerifyEmail";
 
 class App extends Component {
-  handlePersistorState = () => {
-    const { persistor } = this.props;
-    let { bootstrapped } = persistor.getState();
-    if (bootstrapped) {
-      if (this.props.onBeforeLift) {
-        Promise.resolve(this.props.onBeforeLift())
-          .then(() => this.setState({ bootstrapped: true }))
-          .catch(() => this.setState({ bootstrapped: true }));
-      } else {
-        this.setState({ bootstrapped: true });
-      }
+    handlePersistorState = () => {
+        const { persistor } = this.props;
+        let { bootstrapped } = persistor.getState();
+        if (bootstrapped) {
+            if (this.props.onBeforeLift) {
+                Promise.resolve(this.props.onBeforeLift())
+                    .then(() => this.setState({ bootstrapped: true }))
+                    .catch(() => this.setState({ bootstrapped: true }));
+            } else {
+                this.setState({ bootstrapped: true });
+            }
+        }
+    };
+
+    componentDidMount() {
+        this.handlePersistorState();
     }
-  };
 
-  componentDidMount() {
-    this.handlePersistorState();
-  }
+    render() {
+        return (
+            <Fragment>
+                <Router history={history}>
+                    <div className="main-container">
+                        <div className="content-container">
+                            <CustomScrollbars
+                                style={{ height: "100vh", width: "100%" }}
+                            >
+                                <Switch>
+                                    <Route
+                                        path={path.HOME}
+                                        exact
+                                        component={Home}
+                                    />
+                                    <Route
+                                        path={path.LOGIN}
+                                        component={userIsNotAuthenticated(
+                                            Login
+                                        )}
+                                    />
+                                    <Route
+                                        path={path.SYSTEM}
+                                        component={userIsAuthenticated(System)}
+                                    />
+                                    <Route
+                                        path={path.HOMEPAGE}
+                                        component={HomePage}
+                                    />
 
-  render() {
-    return (
-      <Fragment>
-        <Router history={history}>
-          <div className="main-container">
-            <div className="content-container">
-              <CustomScrollbars style={{ height: "100vh", width: "100%" }}>
-                <Switch>
-                  <Route path={path.HOME} exact component={Home} />
-                  <Route
-                    path={path.LOGIN}
-                    component={userIsNotAuthenticated(Login)}
-                  />
-                  <Route
-                    path={path.SYSTEM}
-                    component={userIsAuthenticated(System)}
-                  />
-                  <Route path={path.HOMEPAGE} component={HomePage} />
+                                    <Route
+                                        path={path.DETAIL_PRODUCT}
+                                        component={DetailProduct}
+                                    />
 
-                  <Route path={path.DETAIL_PRODUCT} component={DetailProduct} />
+                                    <Route
+                                        path={path.DETAIL_TECHNOLOGY}
+                                        component={DetailTechnology}
+                                    />
 
-                  <Route
-                    path={path.DETAIL_TECHNOLOGY}
-                    component={DetailTechnology}
-                  />
+                                    <Route
+                                        path={path.DETAIL_POSTS}
+                                        component={DetailPosts}
+                                    />
 
-                  <Route path={path.DETAIL_POSTS} component={DetailPosts} />
+                                    <Route
+                                        path={path.DETAIL_PROJECT}
+                                        component={DetailProject}
+                                    />
 
-                  <Route path={path.DETAIL_PROJECT} component={DetailProject} />
+                                    <Route
+                                        path={path.GET_ALL_PRODUCT}
+                                        component={GetAllProduct}
+                                    />
 
-                  <Route
-                    path={path.GET_ALL_PRODUCT}
-                    component={GetAllProduct}
-                  />
+                                    <Route
+                                        path={path.GET_ALL_PROJECT}
+                                        component={GetAllProject}
+                                    />
 
-                  <Route
-                    path={path.GET_ALL_PROJECT}
-                    component={GetAllProject}
-                  />
+                                    <Route
+                                        path={path.GET_ALL_POSTS}
+                                        component={GetAllPosts}
+                                    />
 
-                  <Route path={path.GET_ALL_POSTS} component={GetAllPosts} />
+                                    <Route
+                                        path={path.VERIFY_EMAIL_BOOKING}
+                                        component={VerifyEmail}
+                                    />
+                                </Switch>
+                            </CustomScrollbars>
+                        </div>
 
-                  <Route
-                    path={path.VERIFY_EMAIL_BOOKING}
-                    component={VerifyEmail}
-                  />
-                </Switch>
-              </CustomScrollbars>
-            </div>
-
-            {/* <ToastContainer
+                        {/* <ToastContainer
               className="toast-container"
               toastClassName="toast-item"
               bodyClassName="toast-item-body"
@@ -105,32 +128,32 @@ class App extends Component {
               closeButton={<CustomToastCloseButton />}
             /> */}
 
-            <ToastContainer
-              position="top-right"
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-            />
-          </div>
-        </Router>
-      </Fragment>
-    );
-  }
+                        <ToastContainer
+                            position="top-right"
+                            autoClose={5000}
+                            hideProgressBar={false}
+                            newestOnTop={false}
+                            closeOnClick
+                            rtl={false}
+                            pauseOnFocusLoss
+                            draggable
+                            pauseOnHover
+                        />
+                    </div>
+                </Router>
+            </Fragment>
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-  return {
-    started: state.app.started,
-  };
+    return {
+        started: state.app.started,
+    };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+    return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
